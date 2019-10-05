ef95247f5831eaed19309f7753fee17f34a1ccf6
vincent Onyango <verencelola@icloud.com>
2019-10-05 18:21:59 +0300

    - allow users to register/ signup using the service  - allow users to login and be authenticated using JWT

---
+++ shoppingList/apps/account/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    define custom user manager
+    """
+    
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users must have a username')
+        if email is None:
+            raise TypeError('Users must have an email')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, username, email, password):
+        """
+        create a super user
+        """
+        if not password:
+            raise TypeError("Superuser must have a password")
+        user = self.create_user(username, email, password=password)
+        user.is_superuser = True
+        user.save()
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        """
+        return username when User object is printed
+        """
+        return self.username
+
+    @property
+    def get_full_name(self):
+        """
+        return users username
+        """
+        return self.username
+
+    
---
+++ shoppingList/helpers/response.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+
+def success_response(message, data, **kwargs):
+    """
+    return a formated response if request successful
+    """
+    return Response({
+        "status": "success",
+        "message": message,
+        "data": data,
+    }, kwargs.get('status_code'))
+
+def error_response(message, **kwargs):
+    return Response(({
+         "status": "error",
+         "message": message,
+     }, kwargs.get('status_code')))
